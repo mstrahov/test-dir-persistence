121e632c217e7b163e507f695fd5f633d8936582
misha <misha@debian-BULLSEYE-live-builder-AMD64>
2025-02-07 00:29:25 +0300

    20250206

--- js/dftransforms.js
+++ js/dftransforms.js
@@ -19,7 +19,7 @@ export const TransformStep = {
 	executionTime: 0,
 };
 
-
+//  methods: toJson(), load from json
 export const TransformScript = {
 	srcfiles: [],
 	destfiles: [],
@@ -32,3 +32,74 @@ export const TransformScript = {
 	lastRunResult: "",
 	executionTime: 0,
 };
+
+export class TransformStepsControl {
+	#containerid;
+	#containertemplateid;
+	#internalContainer;
+	#uuid;
+	constructor(params) {
+		this.#containerid=params.containerid;	
+		this.#uuid = self.crypto.randomUUID();
+		this.#internalContainer = document.querySelector(this.#containerid);
+		this.#containertemplateid=params.containertemplateid;
+		const template = document.querySelector(this.#containertemplateid);
+		const clone = template.content.cloneNode(true);
+		clone.querySelector("#loadstepsfromdisk").id = "loadstepsfromdisk"+this.#uuid;
+		clone.querySelector("#savestepstodisk").id = "savestepstodisk"+this.#uuid;
+		clone.querySelector("#transformstable").id = "transformstable"+this.#uuid;
+		
+		this.#internalContainer.appendChild(clone);
+		
+		this.#internalContainer.querySelector("#loadstepsfromdisk"+this.#uuid).addEventListener("click", this.btnLoadStepsFromDiskClick.bind(this));
+		this.#internalContainer.querySelector("#savestepstodisk"+this.#uuid).addEventListener("click", this.btnSaveStepsToDiskClick.bind(this));
+		
+		this.tabulatorProperties = {
+			height:"311px", 
+			rowHeader:{headerSort:false, resizable: false, minWidth:30, width:30, rowHandle:true, formatter:"handle"},
+			columns:[
+				{title:"Name", field:"srcDfActionName", editor:true,},
+				{title:"Status", field:"lastRunStatus", editor:true,},
+				{title:"Type", field:"targetEnv", editor:true,},
+				{title:"Code", field:"scriptCode", editor:true,},
+			],
+			data:[
+				{
+				stepOrder: 0,
+				srcDfActionName: "Import file",
+				scriptCode: "file = pd.ExcelFile('/mount_dir/onlineretail2.xlsx')",
+				targetEnv: "py",
+				targetDataframe: "df",
+				mutations: ["df","file"], 
+				lastRunStatus: "success",
+				lastRunResult: "",
+				executionTime: 0,
+			 },
+			 {
+				stepOrder: 1,
+				srcDfActionName: "Import file as excel",
+				scriptCode: "df = pd.read_excel(file,sheet_name='Sheet2', skiprows=0)",
+				targetEnv: "py",
+				targetDataframe: "df",
+				mutations: ["df","file"], 
+				lastRunStatus: "success",
+				lastRunResult: "",
+				executionTime: 0,
+			 },
+			],
+		};
+		
+		this.tabulatorObj = new Tabulator("#transformstable"+this.#uuid, this.tabulatorProperties);
+		
+		
+	}
+	
+	btnLoadStepsFromDiskClick(e) {
+		console.log("loadstepsfromdisk click");
+	}
+	
+	btnSaveStepsToDiskClick(e) {
+		console.log("savestepstodisk click");
+	}
+	
+}
