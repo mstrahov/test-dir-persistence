c3cc754491ffadc0658b097984d2481901351e83
misha <misha@debian-BULLSEYE-live-builder-AMD64>
2025-02-09 04:41:28 +0300

    20250208-1

--- js/dftransforms.js
+++ js/dftransforms.js
@@ -33,67 +33,78 @@ export const TransformScript = {
 	executionTime: 0,
 };
 
+/*----------
+ * class TransformStepsControl
+ * 
+ * params.containerid -  container element
+ * containertemplateid  - container of a html template
+ * outputcodefunc(codetext,callback?) - call this to send codetext outside of control 
+ * transformscript - transform steps object
+ * 
+ * 
+ * --------*/
 export class TransformStepsControl {
 	#containerid;
 	#containertemplateid;
 	#internalContainer;
 	#uuid;
+	#outputcodefunc; 
+	#getcodefunc; 
+	#tabulatorObj;
+	#transformscript;
+	
 	constructor(params) {
 		this.#containerid=params.containerid;	
 		this.#uuid = self.crypto.randomUUID();
 		this.#internalContainer = document.querySelector(this.#containerid);
 		this.#containertemplateid=params.containertemplateid;
+		this.#outputcodefunc = params.outputcodefunc;
+		this.#getcodefunc = params.getcodefunc;
+		this.#transformscript = {...params.transformscript};
+		
 		const template = document.querySelector(this.#containertemplateid);
 		const clone = template.content.cloneNode(true);
 		clone.querySelector("#loadstepsfromdisk").id = "loadstepsfromdisk"+this.#uuid;
 		clone.querySelector("#savestepstodisk").id = "savestepstodisk"+this.#uuid;
 		clone.querySelector("#transformstable").id = "transformstable"+this.#uuid;
+		clone.querySelector("#editasjson").id = "editasjson"+this.#uuid;
+		clone.querySelector("#savepyscript").id = "savepyscript"+this.#uuid;
+		clone.querySelector("#loadpyscript").id = "loadpyscript"+this.#uuid;
 		
 		this.#internalContainer.appendChild(clone);
 		
 		this.#internalContainer.querySelector("#loadstepsfromdisk"+this.#uuid).addEventListener("click", this.btnLoadStepsFromDiskClick.bind(this));
 		this.#internalContainer.querySelector("#savestepstodisk"+this.#uuid).addEventListener("click", this.btnSaveStepsToDiskClick.bind(this));
+		// editasjson  btnEditAsJsonClick
+		// savepyscript btnSavePyScriptClick
+		// loadpyscript  btnLoadPyScriptClick
+		this.#internalContainer.querySelector("#editasjson"+this.#uuid).addEventListener("click", this.btnEditAsJsonClick.bind(this));
+		this.#internalContainer.querySelector("#savepyscript"+this.#uuid).addEventListener("click", this.btnSavePyScriptClick.bind(this));
+		this.#internalContainer.querySelector("#loadpyscript"+this.#uuid).addEventListener("click", this.btnLoadPyScriptClick.bind(this));
+		
 		
 		this.tabulatorProperties = {
 			height:"311px", 
 			movableRows:true,
+			reactiveData:true, 
+			//index: "stepOrder",
 			rowHeader:{headerSort:false, resizable: false, minWidth:30, width:30, rowHandle:true, formatter:"handle"},
 			columns:[
+				{
+				  formatter: "rownum",
+				  width: 20
+				},
+				{title:"stepOrder", field:"stepOrder", editor:false, headerSort:false,},
 				{title:"Name", field:"srcDfActionName", editor:true, headerSort:false,},
 				{title:"Status", field:"lastRunStatus", editor:true,headerSort:false,},
 				{title:"Type", field:"targetEnv", editor:true,headerSort:false,},
 				{title:"Code", field:"scriptCode", editor:true,headerSort:false,formatter:"textarea",},
 				{title:"Exec time", field:"executionTime", editor:false,headerSort:false,},
 			],
-			data:[
-				{
-				stepOrder: 0,
-				srcDfActionId: "",
-				srcDfActionName: "Import file",
-				scriptCode: "file = pd.ExcelFile('/mount_dir/onlineretail2.xlsx')",
-				targetEnv: "py",
-				targetDataframe: "df",
-				mutations: ["df","file"], 
-				lastRunStatus: "success",
-				lastRunResult: "",
-				executionTime: 0,
-			 },
-			 {
-				stepOrder: 1,
-				srcDfActionId: "",
-				srcDfActionName: "Import file as excel",
-				scriptCode: "df = pd.read_excel(file,sheet_name='Sheet2', skiprows=0)",
-				targetEnv: "py",
-				targetDataframe: "df",
-				mutations: ["df","file"], 
-				lastRunStatus: "success",
-				lastRunResult: "",
-				executionTime: 0,
-			 },
-			],
+			data:this.#transformscript.transformSteps,
 		};
 		
-		this.tabulatorObj = new Tabulator("#transformstable"+this.#uuid, this.tabulatorProperties);
+		this.#tabulatorObj = new Tabulator("#transformstable"+this.#uuid, this.tabulatorProperties);
 		
 		
 	}
@@ -105,5 +116,91 @@ export class TransformStepsControl {
 	btnSaveStepsToDiskClick(e) {
 		console.log("savestepstodisk click");
 	}
+	
+	btnEditAsJsonClick(e) {
+		console.log("editasjson click");
+		this.#outputcodefunc(JSON.stringify(this.#transformscript,null,4));
+		
+	}
+	
+	btnSavePyScriptClick(e) {
+		console.log("savepyscript click");
+		const scriptheader = { ...this.#transformscript, transformSteps: undefined };
+		let pycode = '#---script: ' + JSON.stringify(scriptheader) + "\n";
+		
+		const rows = this.#tabulatorObj.getRows();
+		for (let i=0;i<rows.length;i++) {
+				rows[i].update({"stepOrder":rows[i].getPosition()});	
+		}
+				
+		
+		//let scriptsteps = this.#tabulatorObj.getData();
+		// this.#transformscript.transformSteps
+		let scriptsteps = this.#transformscript.transformSteps;
+		scriptsteps.sort((a,b)=>a.stepOrder-b.stepOrder);
+		for (let i=0;i<scriptsteps.length;i++) {
+			pycode += '#---step '+i+':'+JSON.stringify({ ...scriptsteps[i], scriptCode: undefined }) + "\n";
+			pycode += scriptsteps[i].scriptCode + "\n";
+		}
+		
+		this.#outputcodefunc(pycode);
+	}
+	
+	btnLoadPyScriptClick(e) {
+		console.log("loadpyscript click");
+		let pycode = this.#getcodefunc();
+		//console.log("editor=", pycode);
+		pycode +='#---step';
+		const re = /#-+step +\d+:((.|\n)*?)(?=(#-+step))/gmi;
+		const stepsarr = pycode.match(re);
+		let newstepsarray = [];	
+		if (stepsarr!==null) {
+			
+			for (let i=0;i<stepsarr?.length;i++) {
+				//console.log(stepsarr[i]);
+				const cmdlines = stepsarr[i].split("\n");
+				console.log("cmdlines",cmdlines);
+				const re_json1 = /#-+step +\d+:(.+)/gi;
+				let stepobj = {};
+				stepobj.scriptCode = '';
+				
+				for (let j=0;j<cmdlines.length;j++) {
+					const jsonfound = [...cmdlines[j].matchAll(re_json1)];
+					//console.log("inner json:",jsonfound[0],jsonfound[0].length);
+					if (jsonfound.length>0&&jsonfound[0].length>1) {
+						//console.log("inner json:",jsonfound[0][1]);
+						try { 
+							let stepobjfromjson = JSON.parse(jsonfound[0][1]);
+							stepobj = {...stepobjfromjson};
+							stepobj.scriptCode = '';
+						} catch(e) {
+							console.log('error parsing json for a step:',e,jsonfound[0][1]);
+						}
+					} else {
+						//  treat current line as py script
+						 
+						
+						if ((j<(cmdlines.length-1))||(j===(cmdlines.length-1)&&cmdlines[j].length>0)) {
+							if (stepobj.scriptCode.length>0) {
+								stepobj.scriptCode += "\n";
+							} 
+							stepobj.scriptCode += cmdlines[j];
+						} 
+					}	
+				}
+				// todo:  verify all fields of stepobj???
+				console.log("step obj=",stepobj);
+				newstepsarray.push(stepobj);
+			}
+			this.#transformscript.transformSteps = [...newstepsarray];
+			this.#tabulatorObj.setData(this.#transformscript.transformSteps);
+				
+		} else {
+			console.log("not a script");
+		}
+	}
+	
+	
+	
 	
 }
