010c4b3028b852d4784535df81149c7950407e53
misha <3997680+mstrahov@users.noreply.github.com>
2025-03-15 00:50:20 +0300

    20250314-1

--- js/transformtableview.js
+++ js/transformtableview.js
@@ -13,37 +13,19 @@ export class TransformTableView extends DataFrameTableView {
 			selectableRangeRows:true,	
 			//--------------------
 			spreadsheetColumnDefinition:{
-					//editor:"input",
-					contextMenu: this.generateCellContextMenu(),
-					headerContextMenu:[
-						{
-							label:"Column header context menu",
-							action:function(e, column){
-								column.updateDefinition({title:"Updated Title"});
-								let colIndex = column.getTable().getColumnLayout().findIndex((el)=>el.field===column.getField());
-								console.log(column,"column=",colIndex);
-							}
-						},
-					],
-				},
-				//-------------
-				rowContextMenu:[
-					{
-						label:"Click here Row",
-						action:function(e, row){
-							console.log(e);
-							console.log(row,"row=",row.getIndex());
-						}
-					},
-				],
+				//editor:"input",
+				contextMenu: this.generateCellContextMenu(),
+				headerContextMenu:this.generateColumnHeaderContextMenu(),
+			},
+			//-------------
+			rowContextMenu:this.generateRowContextMenu(),
 		};
 		
 	}
-	
+	// --------------------------------------------
 	generateCellContextMenu() {
 		let res = [];
 		const actionsarr = getdfActionsArray(["cell"]);
-		console.log("actionsarr",actionsarr);
 		const that = this;
 		for (const a of actionsarr) {
 			res.push({
@@ -51,26 +33,42 @@ export class TransformTableView extends DataFrameTableView {
 				action: function(e, cell){
 								let curColumn = cell.getColumn();
 								let colIndex = curColumn.getTable().getColumnLayout().findIndex((el)=>el.field===curColumn.getField());
-								console.log(cell,"row=",cell.getRow().getIndex(),"column=",colIndex);
-								
 								that.eventbus.dispatch('dfActionEvent',that,{actionid:a.actionid, parameters:{df:"df",rownum:cell.getRow().getIndex()-1, colnum:colIndex-1}}  );
 							}
 			});
-			
 		}
-		
 		return res;
 	}
 	
+	// --------------------------------------------
 	generateColumnHeaderContextMenu() {
 		let res = [];
 		const actionsarr = getdfActionsArray(["column"]);
+		const that = this;
+		for (const a of actionsarr) {
+			res.push({
+				label:a.name,
+				action: function(e, column){
+								let colIndex = column.getTable().getColumnLayout().findIndex((el)=>el.field===column.getField());
+								that.eventbus.dispatch('dfActionEvent',that,{actionid:a.actionid, parameters:{df:"df", colnum:colIndex-1}}  );
+						}
+			});
+		}
 		return res;
 	}
-	
+	// --------------------------------------------
 	generateRowContextMenu() {
 		let res = [];
 		const actionsarr = getdfActionsArray(["row"]);
+		const that = this;
+		for (const a of actionsarr) {
+			res.push({
+				label:a.name,
+				action:	function(e, row){
+							that.eventbus.dispatch('dfActionEvent',that,{actionid:a.actionid, parameters:{df:"df",rownum:row.getIndex()-1 }}  );
+						}
+			});	
+		}
 		return res;
 	}
 	
