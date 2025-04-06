46c8e932c0b669320ba162a4c066a3298e6dd12e
misha <3997680+mstrahov@users.noreply.github.com>
2025-04-07 01:01:16 +0300

    20250406-3

--- js/apppagecontrol.js
+++ js/apppagecontrol.js
@@ -1,15 +1,116 @@
 /*******************
- * AppPageControl - manages contents on one page/tab view with grid
+ * AppPageControl - manages contents on one page/tab view with grid 
  * contains grid, can add controls to the grid, can save grid's layout settings
  * can restore grid
  * should have: destroy method - first destroys all elements it contains 
  * 
+ * Requirements:  bootstrap5, gridstack.js
  * 
  * ****************************/
 import EventBus from "./eventbus.js";
+import { TabNavigationControl, BaseTabControl, DropDownTabControl } from "./tabnavigationcontrol.js";
+import { MenuEventsControl } from "./menueventscontrol.js";
 
 export class AppPageControl {
+	constructor (params) {
+		this.uuid = self.crypto.randomUUID();
+		this.eventbus = new EventBus();
+		
+		// tabnavcontrol 
+		this.tabnavcontrol = params.tabnavcontrol;
+		this.baseTabControlType = params.baseTabControlType || BaseTabControl;
+		this.insertBeforePosition = params.insertBeforePosition;
+		
+		// tab header template 
+		this.navitemtemplateid  = params.navitemtemplateid;
+		// tab body template
+		this.templateid = params.templateid;
+		this.tabtitle = params.tabtitle;
+		
+		
+		// add tab with contents
+		
+		this.contenttab = this.tabnavcontrol.addNewTab(this.baseTabControlType, 
+					{
+						insertBeforePosition: this.insertBeforePosition, 
+						templateid: this.templateid, 
+						navitemtemplateid: this.navitemtemplateid, 
+						tabtitle: this.tabtitle ,  
+					});
+		
+		// tab context menu template
+		this.DropDownMenuTemplateID = params.DropDownMenuTemplateID || null;
+		this.dropdownMenuTab = null;
+		this.dropdownMenuControl = null;
+		if (this.DropDownMenuTemplateID) {
+			// add top dropdownmenu
+			let dropdownInsertBeforePos = this.insertBeforePosition; 
+			if (this.insertBeforePosition>0) {
+				dropdownInsertBeforePos++;
+			}; 
+			this.dropdownmenutab = this.tabnavcontrol.addNewTab(DropDownTabControl, {insertBeforePosition:dropdownInsertBeforePos,  navitemtemplateid: this.DropDownMenuTemplateID, });
+			const menutabnavelement = this.dropdownmenutab.tabnavelement;
+			// top dropdown can be toggled by right-click or long-press on tab's title 			
+			this.contenttab.tabnavelement.addEventListener("contextmenu", (event)=>{
+				event.preventDefault();
+				bootstrap.Dropdown.getOrCreateInstance(menutabnavelement)?.toggle();
+			});
+			
+			this.dropdownMenuControl = new MenuEventsControl({dropDownMenuElementId:this.dropdownmenutab.DropDownMenuElementSelector, parentUUID: this.dropdownmenutab.uuid, multiLevelMenu:false});
+			this.dropdownMenuControl.eventbus.subscribe('menuitemclick',this.topDropDownEventHandler.bind(this));
+		
+		}
+		
+		// grid element init  https://github.com/gridstack/gridstack.js/tree/master/doc
+		let opts = {
+			  cellHeight: 'auto', // see other possible values (best to do in here)
+			  cellHeightThrottle: 100,
+			  handle: '.grid-elem-draggable' ,
+			  margin: 1,
+			  cellHeight: 80,
+			  animate: true, // show immediate (animate: true is nice for user dragging though)
+			  columnOpts: {
+				layout: "moveScale",
+				breakpointForWindow: true,  // test window vs grid size
+				breakpoints: [{w:700, c:1}]
+			  },
+			  float: true,
+		}
+		
+		this.grid = GridStack.init(opts,'#gridcontainer'+this.contenttab.uuid);
+		var items = [
+			{w: 6, h:5, content: 'Test widget number one'}, 
+			{w: 6, h:5, content: 'another longer widget!'} 
+		];
+		this.grid.load(items);
+		
+		// list of grid controls on a page
+		this.gridControls = [];
+		
+	}
 	
+	init() {
+		
+	}
 	
+	addGridControl() {
+		
+	}
+	
+	topDropDownEventHandler(obj,eventdata) {
+		console.log("main drop down menu item click",obj,eventdata); 
+		
+	}
+	
+	destroy() {
+		//  dropdownMenuControl - clear eventbus
+		//  dropdownmenutab  - destroy
+		
+	}
+	
+	saveToJSON() {
+	
+		
+	}
 	
 }
--- js/mainapp.js
+++ js/mainapp.js
@@ -5,6 +5,7 @@ import { PyodideLoader } from "./pyodideloader.js";
 import { AppStatusView } from "./appstatusview.js";
 import { TabNavigationControl, BaseTabControl, DropDownTabControl } from "./tabnavigationcontrol.js";
 import { MenuEventsControl } from "./menueventscontrol.js";
+import { AppPageControl }  from "./apppagecontrol.js";
 
 console.log("test main app");
 
@@ -21,10 +22,24 @@ mainMenuControl.eventbus.subscribe('menuitemclick',(obj,eventdata)=>{
 		console.log("mainmenuitemclick",obj,eventdata); 
 	});
 
-tabnavcontrol.addNewTab(BaseTabControl, {insertBeforePosition:-1, templateid: "#emptyTabContentTemplate", navitemtemplateid: "#emptyTabNavItemTemplate", tabtitle: "tab title 1" , tabbody: "tab 1 body here" });
-tabnavcontrol.addNewTab(BaseTabControl, {insertBeforePosition:-1, templateid: "#emptyTabContentTemplate", navitemtemplateid: "#emptyTabNavItemTemplate", tabtitle: "tab title 2" , tabbody: "tab 2 body here" });
+//tabnavcontrol.addNewTab(BaseTabControl, {insertBeforePosition:-1, templateid: "#emptyTabContentTemplate", navitemtemplateid: "#emptyTabNavItemTemplate", tabtitle: "tab title 1" , tabbody: "tab 1 body here" });
+//tabnavcontrol.addNewTab(BaseTabControl, {insertBeforePosition:-1, templateid: "#emptyTabContentTemplate", navitemtemplateid: "#emptyTabNavItemTemplate", tabtitle: "tab title 2" , tabbody: "tab 2 body here" });
 
 const appstatusview = new AppStatusView({templateid: "#statusdisplaycontroltemplate", containerid: tabNavStatusTab.TabNavTitleElementSelector });
+
+// newAppMenu templates :  #menuAppTab01
+const newAppPage = new AppPageControl( { 
+			tabnavcontrol: tabnavcontrol,  
+			baseTabControlType:BaseTabControl, 
+			insertBeforePosition:-1, 
+			templateid: "#emptyTabContentTemplate", 
+			navitemtemplateid: "#emptyTabNavItemTemplate", 
+			tabtitle: "New App Page 1" , 
+			DropDownMenuTemplateID: "#menuAppTab01",
+		});
+
+
+
 // =====  duckdb & pyodide
 
 window.duckdb = new DuckDBLoader();
