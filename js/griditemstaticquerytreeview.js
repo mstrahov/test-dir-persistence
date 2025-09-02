73766ab306170a75ea0e3a7115dd5e89b75ec741
misha <3997680+mstrahov@users.noreply.github.com>
2025-09-02 04:42:53 +0300

    20250901-1

--- js/griditemstaticquerytreeview.js
+++ js/griditemstaticquerytreeview.js
@@ -4,22 +4,23 @@
  * 
  * 
  ***********************************   */
-
+// https://www.npmjs.com/package/@shopify/draggable/v/1.1.4?activeTab=readme
+// https://cdn.jsdelivr.net/npm/@shopify/draggable@1.1.4/build/
+// https://github.com/Shopify/draggable
+//  /home/misha/work/dev/analysis_project2/v_demo4/js/monitorlib.js 
 
 import { GridItemWithMenu } from "./griditemwithmenu.js";
 import { ExecTimer } from "./exectimer.js"; 
 import { gridItemQueryView, arrowDataTypesToTabulatorCols } from "./griditemqueryview.js";
 
 export class gridItemStaticQueryTreeView extends gridItemQueryView {
-	#internalContainer;
-	#defer;
-	#resolve;
-	#reject;
 	
 	constructor (params) {
 		super(params);	
 		this.displaymode = this._DATATREE;  
 		this.groupFieldsList = [];
+		this.sqlNormalized = null;
+		this.groupingIDFieldName = '';
 	}
 	
 
@@ -33,7 +34,7 @@ export class gridItemStaticQueryTreeView extends gridItemQueryView {
 		// need to find this._dataTreeElementColumn ?
 		if (!this.groupFieldsList || this.groupFieldsList.length==0) {
 			
-			let analyzeSqlCmd = `select json_serialize_sql('${escapedSqlCmd}')->'$.statements[*].*.group_expressions[*].column_names[*]' as r1;`;
+			let analyzeSqlCmd = `select json_serialize_sql('${escapedSqlCmd}')->'$.statements[*].*.group_expressions[*].column_names[*]' as f1;`;
 			let resAnalyze = await this.coderunner.runSQLAsync(analyzeSqlCmd);
 			if (!resAnalyze?.runStatus) {
 				console.error("Tree query view update error during query analysis:", resAnalyze.error);
@@ -41,7 +42,7 @@ export class gridItemStaticQueryTreeView extends gridItemQueryView {
 				return false;
 			}
 			try {
-				this.groupFieldsList = JSON.parse(resAnalyze.output);
+				this.groupFieldsList = JSON.parse(resAnalyze.output?.get(0)['f1']);
 				// if not Array.isArray				
 			} catch (err) {
 				console.error("Tree query view group fields parsing error:", err);
@@ -50,8 +51,25 @@ export class gridItemStaticQueryTreeView extends gridItemQueryView {
 			}
 		}
 		
-		//  select json_deserialize_sql(json_serialize_sql(`${escapedSqlCmd}`)) as r1;
-		
+		if (!this.sqlNormalized) {
+			//  select json_deserialize_sql(json_serialize_sql(`${escapedSqlCmd}`)) as f1;
+			let analyzeSqlCmd = `select json_deserialize_sql(json_serialize_sql('${escapedSqlCmd}')) as f1;`;
+			let resAnalyze = await this.coderunner.runSQLAsync(analyzeSqlCmd);
+			if (!resAnalyze?.runStatus) {
+				console.error("Tree query sql query analysis error:", resAnalyze.error);
+				this.eventbus.dispatch('CmdExecutionError', this, { targetEnv: 'sql', cmd:analyzeSqlCmd, result: resAnalyze });
+				return false;
+			}
+			try {
+				this.sqlNormalized = resAnalyze.output?.get(0)['f1'];
+				// if not Array.isArray				
+			} catch (err) {
+				console.error("Tree query sql query analysis error:", err);
+				this.eventbus.dispatch('CmdExecutionError', this, { targetEnv: 'sql', cmd:resAnalyze.output, result: resAnalyze });
+				return false;
+			}
+		}
+				
 		let strAllFields = this.groupFieldsList.join();
 		// if no GROUPING_ID ?? - guess have to have it or attempt to insert after the last select??
 		// find the last group by.  s1.substr(0,s1.lastIndexOf('GROUP BY')) + 'GROUP BY ' + ....
@@ -61,9 +79,19 @@ export class gridItemStaticQueryTreeView extends gridItemQueryView {
 		/*
 		s1.substr(s1,s1.indexOf('GROUPING('))+ 'GROUPING(' + ..... + s1.substr(s1.indexOf(') ',s1.indexOf('GROUPING(')))
 		*/
-		let modifiedSql = this.sqlcommand;
+		let s1 = this.sqlNormalized;
+		// grouping_id() function should have 'as name' alias in query
+		this.groupingIDFieldName = s1.substr(s1.indexOf('AS',s1.indexOf('GROUPING('))+3,s1.indexOf(',',s1.indexOf('AS',s1.indexOf('GROUPING(')))-s1.indexOf('AS',s1.indexOf('GROUPING('))-3);
+		s1 = s1.substr(s1,s1.indexOf('GROUPING('))+ 'GROUPING(' + strAllFields + s1.substr(s1.indexOf(') ',s1.indexOf('GROUPING(')));
+		s1 = s1.substr(0,s1.lastIndexOf('GROUP BY')) + 'GROUP BY GROUPING SETS (';
+		for (let i=0;i<this.groupFieldsList.length;i++) {
+			s1 = s1 + '(' + this.groupFieldsList.slice(0,i+1).join() + '),';
+		}
+		s1 = s1 + ')  ORDER BY ' + strAllFields + ', ' + this.groupingIDFieldName + ';';
 		
-		const res = await this.coderunner.runSQLAsync(modifiedSql);
+		console.log("Modified sql: ",s1);
+		
+		const res = await this.coderunner.runSQLAsync(s1);
 		if (res?.runStatus) {
 			await this.showQueryResult(res.output);
 		} else {
@@ -83,6 +111,10 @@ export class gridItemStaticQueryTreeView extends gridItemQueryView {
 		let resArray = [];
 		// **********
 		
+		// [a,b,c,d], len=4
+		// (1<<3)-1 = grouping_id()=7
+		// from grouping id: Math.log2(7+1)+1 = 4
+		 
 		if (!arrowdata.schema.fields[0].fldname) {
 			let fldcount = {};
 			for (let i=0;i<arrowdata.numCols;i++) {
